0f35c666ec9d310133b9dc96d7ad46e276c4c443
Pâmela Rosa <pamela.cimirro@gmail.com>
2025-11-17 21:22:28 -0300

    add inserts e selects

--- pi_ads.sql
+++ pi_ads.sql
@@ -116,10 +116,53 @@ CREATE TABLE matricula (
 --  COMANDOS DML PARA INSERÇÃO BÁSICA
 -- =========================================================
 INSERT INTO pessoa_fisica (nome, email, telefone, cpf) VALUES ('Airon Valentim', 'airon@senac.com', '11 912341234', '123.456.789-00');
+INSERT INTO endereco (logradouro, numero, complemento, cidade, estado, cep)
+VALUES 
+('Rua das Flores', '120', 'Apto 12', 'São Paulo', 'SP', '01010-000'),
+('Av. Brasil', '450', NULL, 'Rio de Janeiro', 'RJ', '20030-000'),
+('Rua Central', '98', 'Casa', 'Curitiba', 'PR', '80020-000');
+
+INSERT INTO pessoa_juridica (cnpj, razao_social, nome_fantasia, email, telefone, endereco_id)
+VALUES
+('12.345.678/0001-99', 'Tech Solutions LTDA', 'TechSol', 'contato@techsol.com', '11 90000-1234', 1),
+('98.765.432/0001-55', 'Alpha Distribuidora SA', 'AlphaDist', 'vendas@alphadist.com', '21 95555-4444', 2);
+
+INSERT INTO aluno (id, matricula, turno, ano_ingresso, situacao)
+VALUES
+(1, 'A2025001', 'MATUTINO', 2025, 'ATIVO'),
+(2, 'A2025002', 'NOTURNO', 2024, 'TRANCADO');
+
+INSERT INTO professor (id, matricula, formacao)
+VALUES
+(3, 'P2025001', 'Engenharia da Computação');
+
+INSERT INTO fornecedor (id, produto_fornecido, email, telefone)
+VALUES
+(1, 'Computadores e acessórios', 'fornec1@techsol.com', '11 98888-7777'),
+(2, 'Material de escritório', 'fornec2@alphadist.com', '21 97777-8888');
+
+INSERT INTO curso (nome, codigo_curso, duracao_meses)
+VALUES
+('Análise e Desenvolvimento de Sistemas', 'ADS01', 24),
+('Administração', 'ADM01', 36),
+('Redes de Computadores', 'RED01', 24);
+
+INSERT INTO matricula (matricula_numero, aluno_id, curso_id, status)
+VALUES
+('M2025001', 1, 1, 'MATRICULADO'),
+('M2025002', 2, 3, 'CANCELADO');
+
 
 UPDATE pessoa_fisica SET email = 'airon@yahoo.com' WHERE id = 1;
 
-SELECT * FROM pessoa_fisica;	
+SELECT * FROM endereco;
+SELECT * FROM pessoa_fisica;
+SELECT * FROM pessoa_juridica;
+SELECT * FROM aluno;
+SELECT * FROM professor;
+SELECT * FROM fornecedor;
+SELECT * FROM curso;
+SELECT * FROM matricula;
 
 DELETE FROM pessoa_fisica WHERE pessoa_id = 1;		
 	
